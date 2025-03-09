cbfc3c00d907ac9e02f87720a17981b2363be650
Jerome Haltom <jhaltom@alethic.solutions>
2025-03-09 12:12:06 -0500

    .

---
+++ src/IKVM.Jdbc.Data.Tests/JdbcCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Jdbc.Data.Tests
+{
+
+    [TestClass]
+    public class JdbcCommandTests
+    {
+
+        JdbcConnection CreateTestConnection()
+        {
+            return new JdbcConnection(org.sqlite.JDBC.createConnection("jdbc:sqlite:sample.db", new java.util.Properties()));
+        }
+
+    }
+
+}
