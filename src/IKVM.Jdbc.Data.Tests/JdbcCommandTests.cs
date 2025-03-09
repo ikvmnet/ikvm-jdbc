56e73a91493c50c311deba2be0419215059313b2
Jerome Haltom <jhaltom@alethic.solutions>
2025-03-09 12:24:26 -0500

    Add test.

--- src/IKVM.Jdbc.Data.Tests/JdbcCommandTests.cs
+++ src/IKVM.Jdbc.Data.Tests/JdbcCommandTests.cs
@@ -20,6 +20,23 @@ namespace IKVM.Jdbc.Data.Tests
             return new JdbcConnection(org.sqlite.JDBC.createConnection("jdbc:sqlite:sample.db", new java.util.Properties()));
         }
 
+        [TestMethod]
+        public void CanGetColumnNames()
+        {
+            using var cnn = CreateTestConnection();
+            cnn.Open();
+            using var cmd = cnn.CreateCommand();
+            cmd.CommandText = "drop table if exists CanGetColumnNames";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "create table CanGetColumnNames (id integer, name string)";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "select * from CanGetColumnNames";
+            using var rdr = cmd.ExecuteReader();
+            rdr.FieldCount.Should().Be(2);
+            rdr.GetName(0).Should().Be("id");
+            rdr.GetName(1).Should().Be("name");
+        }
+
     }
 
 }
